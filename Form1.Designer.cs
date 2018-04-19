ae0b5150bcfa320f4498c0d4c5577a296014b46b
SoftDev <softwaredevfb@gmail.com>
2018-04-19 17:00:54 -0500

    Initial Commit Redo
    Committed Remaining Files That Didn't Go Through With Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Proj8TempConversation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCelcius = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFahrenheit.Location = new System.Drawing.Point(36, 72);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(231, 30);
+            this.txtFahrenheit.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Temp in Fahrenheit:";
+            // 
+            // lblCelcius
+            // 
+            this.lblCelcius.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCelcius.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCelcius.Location = new System.Drawing.Point(36, 171);
+            this.lblCelcius.Name = "lblCelcius";
+            this.lblCelcius.Size = new System.Drawing.Size(231, 23);
+            this.lblCelcius.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Celcius Temperature: ";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 230);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 35);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(118, 230);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 35);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(225, 230);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 297);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCelcius);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Name = "Form1";
+            this.Text = "Temp Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCelcius;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Developer:  Frankie Barrios
+/// Date:       04/16/2018
+/// Assignment: Proj8TempConversation
+/// Purpose:    Converts Fahrenheit to Celcius
+/// </summary>
+namespace Proj8TempConversation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double C;
+            double F;
+
+            //Take string from F and convert to double for calculation
+            F = double.Parse(txtFahrenheit.Text);
+            //Conversion Calculation
+            C = 5.0 / 9.0 * (F - 32);
+            //Convert C back to a string
+            lblCelcius.Text = C.ToString();
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Clears Celcius
+            lblCelcius.Text = "";
+            //Clears Fahrenheit
+            txtFahrenheit.Text = "";
+            //Takes focus back to fahrenheit textbox
+            txtFahrenheit.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes app
+            Close();
+        }
+    }
+}
